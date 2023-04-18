0f172bfb43112b9a7d1c06ee2d3880f1fd24ed0d
Myltiane <myltiane.aux@gmail.com>
2023-04-17 22:20:04 -0300

    feat: adicionando autheticate com integrações

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,6 +2,32 @@
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 import clientPromise from "./lib/mongodb";
 import bcrypt from "bcrypt";
-export default NextAuth({
+import db from '../../../utils/db';
 
+db.connectDb();
+export default NextAuth({
+    adapter: MongoDBAdapter(clientPromise),
+    providers: [
+        GitHubProvider({
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_SECRET,
+          }),
+          TwitterProvider({
+            clientId: process.env.TWITTER_ID,
+            clientSecret: process.env.TWITTER_SECRET,
+          }),
+          FacebookProvider({
+            clientId: process.env.FACEBOOK_ID,
+            clientSecret: process.env.FACEBOOK_SECRET,
+          }),
+          GoogleProvider({
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_SECRET,
+          }),
+          Auth0Provider({
+            clientId: process.env.AUTH0_CLIENT_ID,
+            clientSecret: process.env.AUTH0_CLIENT_SECRET,
+            issuer: process.env.AUTH0_ISSUER,
+          }),
+    ]
 });
